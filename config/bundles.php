3b9fa872e7af0d80caf40a5e0d7614f51d47be70
Damien Harper <damien.harper@gmail.com>
2019-04-15 09:45:10 +0200

    Updated damienharper/doctrine-audit-bundle to 2.2.0 release

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
 ];
