3dbe9e2f0ed843eaaed0d2e04e0a48a2dc81d127
Marcel Pfeiffer <m.pfeiffer@strucnamics.de>
2020-05-01 11:32:06 +0200

    Require the Symfony web server bundle explicitely

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['all' => true],
 ];
